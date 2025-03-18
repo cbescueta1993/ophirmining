fae09de2ac8dd5f59becc9063059decddde77d3d
cbescueta <cbescueta@davao-water.gov.ph>
2025-03-18 15:17:03 +0800

    TIME

--- createorder.php
+++ createorder.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Manila');
 ignore_user_abort(true); // Allow script to continue if client disconnects
 $start_time = microtime(true); // Start time
 $config = include 'config.php';
